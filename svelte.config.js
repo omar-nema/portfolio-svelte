1d8bfa854ea68292d3a960dbb830566b45ea556b
Omar Nema <omarwnema@gmail.com>
2022-01-12 16:42:54 -0500

    pls deploy

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,12 @@ const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+    }),
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
