ebf1732b5a0b836e5a0cd608360a06055ca024c2
Omar Nema <omarwnema@gmail.com>
2022-01-13 09:42:41 -0500

    Updates

---
+++ internal/pages/about.svelte-82d28d53.js
@@ -0,0 +1,23 @@
+import{S as re,i as oe,s as se,w as ie,k as p,e as r,t as h,x as de,m as u,c as o,a as s,h as v,d as n,b as m,f as le,y as ce,g as ae,J as e,K as pe,q as ue,N as me,o as fe,U as he,B as ve,V as ge,R as te}from"../chunks/vendor-1827c632.js";import{H as _e}from"../chunks/Header-07c3b71b.js";import"../chunks/Filters.svelte_svelte_type_style_lang-8ca00326.js";import"../chunks/paths-28a87002.js";function ye(ne){let l,x,c,d,g,_,j,z,a,V,M,P,N,F,k,H,q,E,O,B,D,C,J,y,K,R,w,f,U,$,S,A;return l=new _e({}),{c(){ie(l.$$.fragment),x=p(),c=r("div"),d=r("div"),g=r("div"),_=r("span"),j=h("about omar"),z=p(),a=r("div"),V=r("p"),M=p(),P=r("p"),N=h("I am a: technologist, strategist, and educator."),F=p(),k=r("p"),H=h(`Since graduating with a biomedical engineering degree in 2015, I have\r
+        been drawn to creative, socially-minded applications of technology. In\r
+        2016, I moved from a data engineering position to product management in\r
+        pursuit of a more craft-based, human-centered role. From 2016-2021, I\r
+        managed and designed all care-focused applications at Arcadia.io as a\r
+        Senior Product Manager.`),q=p(),E=r("p"),O=h(`Outside of work, I have developed a deep fascination for the interplay\r
+        between data, technology, and the human experience. After creating some\r
+        of my own visualization projects, I decided to to push my craft further\r
+        by enrolling in the Data Visualization program at Parsons School of\r
+        Design.`),B=p(),D=r("p"),C=h(`I am currently finishing up my degree (set to graduate in May 2022),\r
+        doing freelance work, and teaching creative coding course at Parsons.`),J=p(),y=r("p"),K=h(`Note: this website only contains my independent creative tech work.\r
+        Professional product management and freelance work are *not* shown here.`),R=p(),w=r("div"),f=r("a"),U=h("download cv"),this.h()},l(t){de(l.$$.fragment,t),x=u(t),c=o(t,"DIV",{class:!0});var b=s(c);d=o(b,"DIV",{class:!0});var I=s(d);g=o(I,"DIV",{class:!0});var G=s(g);_=o(G,"SPAN",{class:!0});var L=s(_);j=v(L,"about omar"),L.forEach(n),G.forEach(n),z=u(I),a=o(I,"DIV",{class:!0});var i=s(a);V=o(i,"P",{}),s(V).forEach(n),M=u(i),P=o(i,"P",{});var Q=s(P);N=v(Q,"I am a: technologist, strategist, and educator."),Q.forEach(n),F=u(i),k=o(i,"P",{});var T=s(k);H=v(T,`Since graduating with a biomedical engineering degree in 2015, I have\r
+        been drawn to creative, socially-minded applications of technology. In\r
+        2016, I moved from a data engineering position to product management in\r
+        pursuit of a more craft-based, human-centered role. From 2016-2021, I\r
+        managed and designed all care-focused applications at Arcadia.io as a\r
+        Senior Product Manager.`),T.forEach(n),q=u(i),E=o(i,"P",{});var W=s(E);O=v(W,`Outside of work, I have developed a deep fascination for the interplay\r
+        between data, technology, and the human experience. After creating some\r
+        of my own visualization projects, I decided to to push my craft further\r
+        by enrolling in the Data Visualization program at Parsons School of\r
+        Design.`),W.forEach(n),B=u(i),D=o(i,"P",{});var X=s(D);C=v(X,`I am currently finishing up my degree (set to graduate in May 2022),\r
+        doing freelance work, and teaching creative coding course at Parsons.`),X.forEach(n),J=u(i),y=o(i,"P",{style:!0});var Y=s(y);K=v(Y,`Note: this website only contains my independent creative tech work.\r
+        Professional product management and freelance work are *not* shown here.`),Y.forEach(n),i.forEach(n),R=u(I),w=o(I,"DIV",{class:!0});var Z=s(w);f=o(Z,"A",{class:!0,href:!0});var ee=s(f);U=v(ee,"download cv"),ee.forEach(n),Z.forEach(n),I.forEach(n),b.forEach(n),this.h()},h(){m(_,"class","svelte-yw44mp"),m(g,"class","about-header svelte-yw44mp"),le(y,"color","gray"),m(a,"class","about-body"),m(f,"class","download-btn svelte-yw44mp"),m(f,"href","/"),m(w,"class","about-footer"),m(d,"class","about-page svelte-yw44mp"),m(c,"class","content")},m(t,b){ce(l,t,b),ae(t,x,b),ae(t,c,b),e(c,d),e(d,g),e(g,_),e(_,j),e(d,z),e(d,a),e(a,V),e(a,M),e(a,P),e(P,N),e(a,F),e(a,k),e(k,H),e(a,q),e(a,E),e(E,O),e(a,B),e(a,D),e(D,C),e(a,J),e(a,y),e(y,K),e(d,R),e(d,w),e(w,f),e(f,U),A=!0},p:pe,i(t){A||(ue(l.$$.fragment,t),me(()=>{S&&S.end(1),$=ge(c,te,{delay:300,duration:500}),$.start()}),A=!0)},o(t){fe(l.$$.fragment,t),$&&$.invalidate(),S=he(c,te,{duration:300}),A=!1},d(t){ve(l,t),t&&n(x),t&&n(c),t&&S&&S.end()}}}class ke extends re{constructor(l){super();oe(this,l,null,ye,se,{})}}export{ke as default};
